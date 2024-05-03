0cdc93ccceb301258beaddf430be86a11bd158a0
PaulloDS <paulosch14@gmail.com>
2024-05-03 20:46:06 -0300

    chore: add selecaoCandidatos

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,9 +1,35 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidato(1900);
-        analisarCandidato(2000);
-        analisarCandidato(2100);
+        selecaoCandidatos();
     }
+    static void selecaoCandidatos(){
+
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecinados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecinados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecinados++;
+            }else{
+                System.out.println("O candidato " + candidato + " não foi selecionado para a vaga.");
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+   }
+
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
