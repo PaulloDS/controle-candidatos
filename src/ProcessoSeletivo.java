dfe2f2c9a22db1a53f07e0e3549d14bceee6247d
PaulloDS <paulosch14@gmail.com>
2024-05-03 21:03:59 -0300

    chore: add entrandoEmContato

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,9 +1,39 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecionados();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for(String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            }else{
+                System.out.println("Contato realizado com sucesso!");
+            }
+        } while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa.");
+        }else{
+            System.out.println("Não conseguimos contato com " + candidato + ", tentativas máximas excedidas: " + tentativasRealizadas);
+        }
+    }
+
+    	//método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
 
     static void imprimirSelecionados(){
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
